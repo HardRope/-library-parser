88a3f87c0f109c18a83b3787c5b5d4c143d843da
HardRope <61661973+HardRope@users.noreply.github.com>
2022-07-11 00:41:51 +0300

    add check of redirection

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-
 import requests
 
 def create_directory(save_dir='books'):
@@ -8,19 +7,33 @@ def create_directory(save_dir='books'):
     return dir_path
 
 
-def download_book(url, path):
-    response = requests.get(url)
-    response.raise_for_status()
+def check_for_redirect(response):
+    if response.status_code != 200:
+        raise requests.HTTPError
 
-    with open(path, 'wb') as file:
-        file.write(response.content)
+
+def download_book(url, id):
+    params = {'id': id }
+    response = requests.get(url, params=params, allow_redirects=False)
+    response.raise_for_status()
+    return response
 
 
 if __name__ == '__main__':
     dir_path = create_directory()
 
     for id in range(1, 11):
-        url = f'https://tululu.org/txt.php?id={id}'
+        url = f'https://tululu.org/txt.php'
+        response = download_book(url, id)
+        try:
+            check_for_redirect(response)
+        except requests.HTTPError:
+            continue
+
         file_name = str(id) + '.txt'
         save_path = dir_path / file_name
-        download_book(url, save_path)
+
+        with open(save_path, 'wb') as file:
+            file.write(response.content)
+
+
