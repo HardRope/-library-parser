ec6069832547a1b89697bad462417bc197cca72b
HardRope <61661973+HardRope@users.noreply.github.com>
2022-07-11 03:28:18 +0300

    parse title and author of book

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 import requests
 
+from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename as sanitize
+
 def create_directory(save_dir='books'):
     dir_path = Path.cwd() / save_dir
     Path.mkdir(dir_path, parents=True, exist_ok=True)
@@ -19,6 +22,15 @@ def download_book(url, id):
     return response
 
 
+def get_book_info(id):
+    response = requests.get(f'https://tululu.org/b{id}/')
+    '''Return book's name and author'''
+    soup = BeautifulSoup(response.text, 'lxml')
+    name = soup.find('h1')
+    title, author = name.text.split(' \xa0 :: \xa0 ')
+    return sanitize(title), sanitize(author)
+
+
 if __name__ == '__main__':
     dir_path = create_directory()
 
@@ -30,7 +42,8 @@ if __name__ == '__main__':
         except requests.HTTPError:
             continue
 
-        file_name = str(id) + '.txt'
+        title, author = get_book_info(id)
+        file_name = f'{id}. {title}.txt'
         save_path = dir_path / file_name
 
         with open(save_path, 'wb') as file:
