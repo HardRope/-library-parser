3e554005076efda29d86522a63c31642b88b76e6
HardRope <61661973+HardRope@users.noreply.github.com>
2022-08-08 20:52:13 +0300

    parse books urls by genres

--- parse_tululu_catgory.py
+++ parse_tululu_catgory.py
@@ -11,7 +11,36 @@ import requests
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename as sanitize
 
+from .main import check_for_redirect
+
+def get_books_urls(url, response):
+    soup = BeautifulSoup(response, 'lxml')
+    soup_of_books = soup.find_all(class_='d_book')
+
+    parsed_urls = []
+    for book_soup in soup_of_books:
+        book_relative_url = book_soup.find('a')['href']
+        book_url = urljoin(url, book_relative_url)
+        parsed_urls.append(book_url)
+    return parsed_urls
 
 
 if __name__ == '__main__':
-    pass
+    logging.basicConfig(level=logging.INFO)
+    books_urls = []
+
+    for page in range(1, 5):
+        try:
+            url = f'https://tululu.org/l55/{page}/'
+            response = requests.get(url)
+            response.raise_for_status()
+            check_for_redirect(response)
+
+            books_urls.extend(get_books_urls(url, response.text))
+        except requests.ConnectionError:
+            logging.info(f'Проблема подключения. Страница {url} не скачана')
+            continue
+        except requests.HTTPError:
+            logging.info(f'Страница {url} отсутствует на сайте.')
+            continue
+    print(*books_urls, sep = '\n')
