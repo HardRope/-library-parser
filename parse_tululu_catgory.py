ab4505c00587c652967b7663442bbc171a8861c9
HardRope <61661973+HardRope@users.noreply.github.com>
2022-08-08 20:27:45 +0300

    Create parse_tululu_catgory.py

---
+++ parse_tululu_catgory.py
@@ -0,0 +1,17 @@
+import argparse
+import json
+import logging
+import time
+
+from pathlib import Path
+from urllib.parse import urljoin
+
+import requests
+
+from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename as sanitize
+
+
+
+if __name__ == '__main__':
+    pass
